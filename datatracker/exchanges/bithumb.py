8dbfda243c97a50e47a35f6472d8542ae9c9e99d
ericjsy <ericjsy604@gmail.com>
2018-01-21 10:33:24 -0800

    Moved exchange files to new directory.

---
+++ datatracker/exchanges/bithumb.py
@@ -0,0 +1,13 @@
+import psycopg2
+from datetime import datetime
+
+
+class Bithumb():
+    def __init__(self, response):
+        self.response = response
+
+    print("Bithumb")
+
+    def pull_data(self):
+        print("Pulling")
+
