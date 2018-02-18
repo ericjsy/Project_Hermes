15b11e01354fb48eab7c72ea4e5d33716084c9c0
ericjsy <ericjsy604@gmail.com>
2018-02-17 16:28:34 -0800

    Reworked table partition function.

--- datahub/db_setup.sql
+++ datahub/db_setup.sql
@@ -15,7 +15,7 @@ CREATE TABLE binance.raw_data
 ,low_price double precision NOT NULL
 ,volume double precision NOT NULL
 ,quote_volume double precision NOT NULL
-,last_updated timestamp without time zone NOT NULL
+,last_updated timestamp NOT NULL
 ) WITH (OIDS = FALSE);
 
 CREATE OR REPLACE FUNCTION binance.create_partition_and_insert() RETURNS trigger AS
@@ -23,25 +23,29 @@ CREATE OR REPLACE FUNCTION binance.create_partition_and_insert() RETURNS trigger
     DECLARE
       partition_date TEXT;
       partition_date_offset TEXT;
-      partition_location TEXT;
+      partition_schema TEXT;
       partition TEXT;
       partition_offset TEXT;
+      partition_table TEXT;
+      partition_table_offset TEXT;
     BEGIN
       partition_date := to_char(NEW.last_updated,'YYYY_MM');
       partition_date_offset := to_char(NEW.last_updated + interval '-1 year','YYYY_MM');
-      partition_location := TG_TABLE_SCHEMA || '.' || TG_RELNAME;
-      partition := partition_location || '_' || partition_date;
-      partition_offset := TG_RELNAME || '_' || partition_date_offset;
-      IF EXISTS (SELECT relname FROM pg_class WHERE relname=partition_offset) THEN
-        RAISE NOTICE 'Inheritence revoked from table patition in previous year: %.', partition_offset;
-        EXECUTE 'ALTER TABLE ' || partition_location || '_' || partition_date_offset || ' NO INHERIT ' || partition_location || ';';
+      partition_schema := TG_TABLE_SCHEMA || '.' || TG_RELNAME;
+      partition := partition_schema || '_' || partition_date;
+      partition_offset := partition_schema || '_' || partition_date_offset;
+      partition_table := TG_RELNAME || '_' || partition_date;
+      partition_table_offset := TG_RELNAME || '_' || partition_date_offset;
+      IF EXISTS (SELECT relname FROM pg_class WHERE relname = partition_table_offset) THEN
+        RAISE NOTICE 'Inheritence revoked from table patition in previous year: %.', partition_table_offset;
+        EXECUTE 'ALTER TABLE ' || partition_offset || ' NO INHERIT ' || partition_schema || ';';
       END IF;
-      IF NOT EXISTS(SELECT relname FROM pg_class WHERE relname=partition) THEN
+      IF NOT EXISTS (SELECT relname FROM pg_class WHERE relname = partition_table) THEN
         RAISE NOTICE 'A partition has been created: %', partition;
-        EXECUTE 'CREATE TABLE ' || partition || ' (check (last_updated = ''' || NEW.last_updated || ''')) INHERITS (' || partition_location || ');';
-        EXECUTE 'CREATE INDEX ' || TG_RELNAME || '_' || partition_date || '_idx ON ' || partition || '(last_updated);';
+        EXECUTE 'CREATE TABLE ' || partition || ' (check ((date_trunc(''day'', last_updated)::DATE >= date_trunc(''month'', timestamp ''' || NEW.last_updated || ''')::DATE) AND (date_trunc(''day'', last_updated)::DATE < date_trunc(''month'', timestamp ''' || NEW.last_updated + interval '1 month' || ''')::DATE))) INHERITS (' || partition_schema || ');';
+        EXECUTE 'CREATE INDEX ' || partition_table || '_idx ON ' || partition || '(last_updated);';
       END IF;
-      EXECUTE 'INSERT INTO ' || partition || ' SELECT(' || partition_location || ' ' || quote_literal(NEW) || ').* RETURNING ID;';
+      EXECUTE 'INSERT INTO ' || partition || ' SELECT(' || partition_schema || ' ' || quote_literal(NEW) || ').* RETURNING ID;';
       RETURN NULL;
     END;
   $BODY$
